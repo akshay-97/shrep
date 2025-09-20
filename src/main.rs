a5da2a41d75bbc3e7e90822cc1f4d7a7f6b0c4fd
akshay.s <adiosphobian@gmail.com>
2025-09-20 20:53:47 +0530

    first stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::io;
 use std::process;
+use clap::{Arg, command};
+use anyhow::{Result,anyhow};
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
@@ -11,24 +13,24 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
-    }
+    let grep = command!()
+        .arg(Arg::new("match_e").short('E'))
+        .get_matches();
 
-    let pattern = env::args().nth(2).unwrap();
+    let pattern = grep.get_one::<String>("match_e").ok_or(anyhow!("Missing Attribute"))?;
+    
+    // let pattern = env::args().nth(2).unwrap();
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
