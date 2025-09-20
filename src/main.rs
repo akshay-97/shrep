4d2449bf63f42d6c969f9fb912722dc816879f95
akshay.s <adiosphobian@gmail.com>
2025-09-20 22:01:15 +0530

    2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::env;
 use std::io;
 use std::process;
-use clap::{Arg, command};
+use clap::{arg, command};
 use anyhow::{Result,anyhow};
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    if pattern == "\\d"{
+        if input_line.chars().any(|c| c.is_ascii_digit()){
+            return true;
+        }
+        false
+    }else{
+        if pattern.chars().count() == 1{
+            return input_line.contains(pattern);
+        }else{
+            panic!("Unhandled pattern")
+        }
     }
+
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -18,11 +26,12 @@ fn main() -> Result<(), anyhow::Error> {
     eprintln!("Logs from your program will appear here!");
 
     let grep = command!()
-        .arg(Arg::new("match_e").short('E'))
+        .about("shit grep -shrep")
+        .arg(arg!(-E <pattern> "give your pattern"))
         .get_matches();
 
-    let pattern = grep.get_one::<String>("match_e").ok_or(anyhow!("Missing Attribute"))?;
-    
+    let pattern = grep.get_one::<String>("pattern").ok_or(anyhow!("Missing Attribute"))?;
+
     // let pattern = env::args().nth(2).unwrap();
     let mut input_line = String::new();
 
