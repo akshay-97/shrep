7f2b956cffdb62634b66e5c3495c26c36f1c4ef9
akshay.s <adiosphobian@gmail.com>
2025-09-21 10:27:14 +0530

    3rd stage

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,51 @@ use std::process;
 use clap::{arg, command};
 use anyhow::{Result,anyhow};
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern == "\\d"{
-        if input_line.chars().any(|c| c.is_ascii_digit()){
-            return true;
-        }
-        false
-    }else{
-        if pattern.chars().count() == 1{
-            return input_line.contains(pattern);
-        }else{
-            panic!("Unhandled pattern")
+enum GrepPatterns<'a>{
+    Number,
+    AlphaNumerUnderscore,
+    Contains(&'a str),
+    Default
+}
+
+impl <'a> GrepPatterns<'a>{
+    fn find(self, input: &str) -> bool{
+        match self{
+            GrepPatterns::AlphaNumerUnderscore =>
+                input.chars().any(|c| c.is_alphanumeric() || c == '_'),
+            GrepPatterns::Number =>
+                input.chars().any(|c| c.is_ascii_digit()),
+            GrepPatterns::Contains(c) =>
+                input.contains(c),
+            GrepPatterns::Default => false
         }
     }
+}
 
+trait Grep{
+    fn grep<'a>(pattern: &'a str) -> GrepPatterns<'a>;
+}
+
+impl <'b> Grep for GrepPatterns<'b>{
+    fn grep<'a>(pattern: &'a str) -> GrepPatterns<'a>{
+        match pattern{
+            "\\d" => GrepPatterns::Number,
+            "\\w" => GrepPatterns::AlphaNumerUnderscore,
+            a => {
+                if a.chars().count() == 1{
+                    GrepPatterns::Contains(a)
+                }else{
+                    GrepPatterns::Default
+                }
+            }
+        }
+    }
+}
+
+fn match_pattern(input_line: &str, pattern: GrepPatterns<'_>) -> bool
+where
+{
+    pattern.find(input_line)
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -37,7 +68,9 @@ fn main() -> Result<(), anyhow::Error> {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, &pattern) {
+    let grep_pattern = GrepPatterns::grep(pattern.as_str());
+
+    if match_pattern(&input_line, grep_pattern) {
         process::exit(0)
     } else {
         process::exit(1)
